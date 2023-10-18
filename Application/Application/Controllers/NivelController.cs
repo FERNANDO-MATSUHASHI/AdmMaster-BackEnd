8f98c2eec095279a5ddf492cddccfd0946515242
Kaue <kaue.correia>
2023-10-18 09:42:12 -0300

    [Kaue] - Ajustes 2.0

--- Application/Application/Controllers/NivelController.cs
+++ Application/Application/Controllers/NivelController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
     public class NivelController : ControllerBase
     {
         [HttpGet]
