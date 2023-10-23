11adb8011fe9c8063b6c8088bb81f08fd8705807
Kaue <kaue.correia>
2023-10-23 09:13:39 -0300

    .

--- Domain/Domain.Service/TokenService.cs
+++ Domain/Domain.Service/TokenService.cs
@@ -1,16 +1,11 @@
 ﻿using Domain.Entites;
 using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
 using System.Security.Claims;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Service
 {
-
     public static class TokenService
     {
         public static string GerarToken(Usuario user)
@@ -32,4 +27,3 @@ namespace Domain.Service
         }
     }
 }
-
