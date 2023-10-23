84a977b21ecddab1871f6f9a57a160fd401c7fb9
Denis Honorio <denishonorio7@gmail.com>
2023-10-23 03:01:20 -0300

    criação do servico de login

---
+++ Domain/Domain.Service/TokenService.cs
@@ -0,0 +1,35 @@
+﻿using Domain.Entites;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Service
+{
+
+    public static class TokenService
+    {
+        public static string GerarToken(Usuario user)
+        {
+            var manipuladorToke = new JwtSecurityTokenHandler();
+            var chave = Encoding.ASCII.GetBytes(Settings.Secret);
+            var descritorToken = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Username.ToString()),
+                    new Claim(ClaimTypes.Role, user.Role.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(2),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
+
