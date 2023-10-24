60be136528c828dfa2bbf0c687aa9a121869effc
Denis Honorio <denishonorio7@gmail.com>
2023-10-24 03:35:40 -0300

    arrumando token

--- Domain/Domain.Service/TokenService.cs
+++ Domain/Domain.Service/TokenService.cs
@@ -16,8 +16,16 @@ namespace Domain.Service
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
+                    //você pode usar a reivindicação ClaimTypes.Name para armazenar o nome
+                    //de usuário do usuário autenticado. Quando o token é verificado, você
+                    //pode acessar essa reivindicação para obter o nome do usuário e usá - lo
+                    //para personalizar a experiência do usuário ou tomar decisões com base no nome de usuário.
                     new Claim(ClaimTypes.Name, user.user_name.ToString()),
-                    new Claim(ClaimTypes.Role, user.senha.ToString())
+                    //você pode usar a reivindicação ClaimTypes.Role para atribuir uma ou várias funções a um usuário.
+                    //As funções podem representar papéis específicos dentro do sistema, como "Administrador,"
+                    //"Usuário" ou "Convidado".Quando um usuário faz login e é emitido um token JWT,
+                    //a reivindicação ClaimTypes.Role pode ser usada para incluir informações sobre as funções do usuário.
+                    new Claim(ClaimTypes.Role, user.Cargos.ToString())
                 }),
                 Expires = DateTime.UtcNow.AddHours(2),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(chave), SecurityAlgorithms.HmacSha256Signature)
